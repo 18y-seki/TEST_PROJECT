1a7b04d421893e2020106bd77072a49d2df11bf9
admin <admin@admin-HP>
2018-06-12 15:43:49 +0900

    commit03 - takeda

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,4 +1,8 @@
+import testpackage.MessageClass;
 
 public class TestClass {
-
+	public static void main(String args[]) {
+		MessageClass MsgClass = new MessageClass();
+		MsgClass.msg();
+	}
 }
---
+++ src/testpackage/MessageClass.java
@@ -0,0 +1,8 @@
+package testpackage;
+
+public class MessageClass {
+
+	public void msg() {
+		System.out.println("Hello World!");
+	}
+}
